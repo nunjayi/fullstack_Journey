c3ec6dc7f8ab439a4d531832832ac663c9330b4e
nunjayi <epenyash@gmail.com>
2024-01-01 12:35:55 +0300

    fixing output files sharing same path but have different contents

--- WEB-DEV-CODY/part2--/src/main.jsx
+++ WEB-DEV-CODY/part2--/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Notes from './forms.jsx'
-import ComplexState from './state.jsx'
-import ObjectButton from './object_spread.jsx'
+
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ComplexState />
-    <ObjectButton />
-    <Notes />
+    
   </React.StrictMode>,
 )
